82db65aa60f7fe34f44e1946552f1fce369b92a2
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-11-04 22:30:57 +0100

    feat: add tests and then method to validate user

--- __tests__/GitHubSDK.test.js
+++ __tests__/GitHubSDK.test.js
@@ -11,24 +11,50 @@ describe('GitHubSDK class', () => {
             };
             expect(createGhSdk).toThrow();
         });
+
         it('Should throw exception when token is not specified', () => {
             function createGhSdk() {
                 new GitHubSDK('xxxxx', undefined);
             };
             expect(createGhSdk).toThrow();
         });
+
         it('Should throw exception when token & username is not specified', () => {
             function createGhSdk() {
                 new GitHubSDK('xxxxx', undefined);
             };
             expect(createGhSdk).toThrow();
         });
+
         it('Should set username & token when create instance', () => {
             const username = 'xxxxx';
             const token = '12345678';
             const ghSdk = new GitHubSDK(username, token);
             expect(ghSdk.getUsername()).toBe(username);
             expect(ghSdk.getToken()).toBe(token);
+        });
+    });
+
+    describe('validateUser()', () => {
+        it('Should throw exception when token is incorrect', async () => {
+            const username = 'szymonsuchanowski';
+            const token = 'invalidtoken';
+            const ghSdk = new GitHubSDK(username, token);
+            await expect(ghSdk.validateUser()).rejects.toThrow('Unauthorized token!');
+        });
+
+        it('Should return status 200 when token is correct & connected with specified username', async () => {
+            const username = user.username;
+            const token = user.token;
+            const ghSdk = new GitHubSDK(username, token);
+            await expect(ghSdk.validateUser()).resolves.toBe(200);
+        });
+
+        it('Should throw exception when token is correct but not connected with specifiedd username', async () => {
+            const username = 'usernamenotconnectedwithtoken';
+            const token = user.token;
+            const ghSdk = new GitHubSDK(username, token);
+            await expect(ghSdk.validateUser()).rejects.toThrow('Token is not connected with specified username!');
         })
     })
 })
--- src/js/GitHubSDK.js
+++ src/js/GitHubSDK.js
@@ -1,3 +1,5 @@
+import user from "./user.config";
+
 class GitHubSDK {
     constructor(usernameValue = null, tokenValue = null) {
         this._setProperty('username', usernameValue);
@@ -12,8 +14,26 @@ class GitHubSDK {
         return this.token;
     }
 
+    async validateUser() {
+        const promise = await fetch('https://api.github.com/user', {
+            headers: {
+                Authorization: `token ${this.token}`,
+            },
+        });
+        if (promise.ok) {
+            const response = await promise.json();
+            if (response.login === this.username) {
+                return promise.status;
+            }
+            throw new Error('Token is not connected with specified username!');
+        } else if (promise.status === 401) {
+            throw new Error('Unauthorized token!');
+        }
+        throw new Error(promise.status);
+    }
+
     _setProperty(propertyName, propertyValue) {
-        if(!propertyValue) {
+        if (!propertyValue) {
             throw new Error(`No ${propertyName} specified!`);
         }
         this[propertyName] = propertyValue;
