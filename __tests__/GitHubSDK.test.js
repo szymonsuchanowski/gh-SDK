b907b64df5854a6195ead96a92e17271074cdb06
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-11-04 20:12:29 +0100

    feat: add test when username is not specified

--- __tests__/GitHubSDK.test.js
+++ __tests__/GitHubSDK.test.js
@@ -3,4 +3,15 @@ global.fetch = nodeFetch;
 import GitHubSDK from '../src/js/GitHubSDK';
 import user from '../src/js/user.config';
 
+describe('GitHubSDK class', () => {
+    describe('Create instance', () => {
+        it('Should throw exception when username is not specified', () => {
+            function createGhSdk() {
+                new GitHubSDK(undefined, 'xxxx');
+            }
+            expect(createGhSdk).toThrow();
+        });
+    })
+})
+
 
