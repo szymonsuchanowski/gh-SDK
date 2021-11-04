485d3e21ac98f8cdd0b1f799d1de4061234ebcc9
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-11-04 20:25:41 +0100

    feat: add methods setting username & token

--- src/js/GitHubSDK.js
+++ src/js/GitHubSDK.js
@@ -1,11 +1,32 @@
 class GitHubSDK {
-    constructor(username, token) {
-        if(!username) {
-            throw new Error('No username specified!');
-        }
+    constructor(username = null, token = null) {
+        this._setUsername(username);
+        this._setToken(token);
         if(!token) {
             throw new Error('No token specified!');
         }
+    }
+
+    getUsername() {
+        return this.username;
+    }
+
+    getToken() {
+        return this.token;
+    }
+
+    _setUsername(usernameValue) {
+        if(!usernameValue) {
+            throw new Error('No username specified!');
+        }
+        this.username = usernameValue;
+    }
+
+    _setToken(tokenValue) {
+        if(!tokenValue) {
+            throw new Error('No token specified!');
+        }
+        this.token = tokenValue;
     }
 }
 
